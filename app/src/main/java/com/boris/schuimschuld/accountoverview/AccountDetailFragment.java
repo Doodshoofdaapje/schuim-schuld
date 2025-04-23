28ebbc1280297485953687eb837665aeebc27947
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2025-04-23 13:32:04 +0200

    Fixed panning

--- app/src/main/java/com/boris/schuimschuld/accountoverview/AccountDetailFragment.java
+++ app/src/main/java/com/boris/schuimschuld/accountoverview/AccountDetailFragment.java
@@ -1,6 +1,8 @@
 package com.boris.schuimschuld.accountoverview;
 
 import com.boris.schuimschuld.R;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,6 +15,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -27,6 +30,7 @@ import com.yalantis.ucrop.UCrop;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -111,20 +115,22 @@ public class AccountDetailFragment extends Fragment {
         );
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void createPlot(ITransactionManager transactionManager) {
         XYPlot plot = binding.accountDetailChart;
-        Tuple<ArrayList<String>, ArrayList<Integer>> data = transactionManager.countPerMonth(account);
 
+        // Add data
+        Tuple<ArrayList<String>, ArrayList<Integer>> data = transactionManager.countPerMonth(account);
         XYSeries series = new SimpleXYSeries(data._y(), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Maand");
         LineAndPointFormatter seriesFormat =
                 new LineAndPointFormatter(getContext(), R.drawable.line_point_formatter_with_labels);
-
         plot.addSeries(series, seriesFormat);
+
+        // Styling
         plot.getLegend().setVisible(false);
-
         plot.setPlotPadding(10, 10, 10, 10);
-        plot.getGraph().setMargins(30, 20, 30, 30);
-
+        plot.getGraph().setMargins(50, 20, 50, 30);
+        
         plot.setDomainStep(StepMode.INCREMENT_BY_VAL, 1);
         plot.getGraph().getLineLabelInsets().setLeft(-20f);
         plot.getGraph().getLineLabelInsets().setBottom(-20f);
@@ -150,10 +156,16 @@ public class AccountDetailFragment extends Fragment {
                 return null;
             }
         });
+
+        // Set zoom restrictions and starting point
+        int windowSize = 5;
+        int dataSize = data._x().size();
+        int startDomain = Math.max(0, dataSize - windowSize);
+        int endDomain = startDomain + windowSize - 1;
         plot.setRangeBoundaries(0, Collections.max(data._y()), BoundaryMode.FIXED);
-        plot.setDomainBoundaries(0, 5, BoundaryMode.FIXED);
+        plot.setDomainBoundaries(startDomain, endDomain, BoundaryMode.FIXED);
+        plot.getOuterLimits().set(0, data._x().size() - 1, 0, Collections.max(data._y()));
         PanZoom.attach(plot, PanZoom.Pan.HORIZONTAL, PanZoom.Zoom.NONE);
-        //plot.getOuterLimits().set(0, data._x().size() - 1, 0, Collections.max(data._y()));
 
     }
 
